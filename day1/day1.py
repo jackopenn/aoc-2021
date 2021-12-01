575f829840a44d253d99b6af27706b75ccb826e1
Jack Pennington <jpennington@expediagroup.com>
2021-12-01 11:57:49 +0000

    day1 cleaned

--- day1/day1.py
+++ day1/day1.py
@@ -1,5 +1,6 @@
-with open("input.txt") as file:
+with open("day1/input.txt") as file:
     nums = [int(line) for line in file.readlines()]
+
 
 nums = [sum(nums[i-2:i+1]) for i in range(2, len(nums))]
 
@@ -8,9 +9,5 @@ count = 0
 for i in range(1, len(nums)):
     if nums[i] > nums[i-1]:
         count += 1
-
-
-
-
 
 print(count)
