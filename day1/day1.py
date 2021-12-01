9fd1ee3a2c6b52bc4c214d9535c109397e7e4386
Jack Pennington <jpennington@expediagroup.com>
2021-12-01 11:42:18 +0000

    day1

---
+++ day1/day1.py
@@ -0,0 +1,16 @@
+with open("input.txt") as file:
+    nums = [int(line) for line in file.readlines()]
+
+nums = [sum(nums[i-2:i+1]) for i in range(2, len(nums))]
+
+count = 0
+
+for i in range(1, len(nums)):
+    if nums[i] > nums[i-1]:
+        count += 1
+
+
+
+
+
+print(count)
---
+++ rng/rng.py
@@ -0,0 +1,11 @@
+import random
+
+langs = ["Python",
+        "Haskell",
+        "Java",
+        "Scala",
+        "JavaScript",
+        "C"
+        "C++",
+        "C#",
+        ]
