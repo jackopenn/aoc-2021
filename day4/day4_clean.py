5353f65e9267f8e64153626594d4817042e85f8c
Jack Pennington <jackcpennington@gmail.com>
2021-12-04 13:16:26 +0000

    further clean

--- day4/day4_clean.py
+++ day4/day4_clean.py
@@ -1,36 +1,26 @@
 with open("day4/input.txt", 'r') as f:
     lines = [line.rstrip().split() for line in f.readlines()]
 
-values = lines[0][0].split(',')
-boards = [lines[idx:idx+6][1:] for idx in range(1, len(lines), 6)]
-
-
-def check_board(board):
-    for row in board:
-        if row.count('X') == len(board):
-            return True
-    for col in zip(*board):
-        if col.count('X') == len(board):
-            return True
-    return False
-
+values = map(int, lines[0][0].split(','))
+boards = [[[int(elem) for elem in row] for row in board] for board in [lines[idx:idx+6][1:] for idx in range(1, len(lines), 6)]]
 
 bingo_boards = []
 
 for v in values:
-    for idx, board in enumerate(boards):
+    for board in boards:
         if board not in [board for _,board in bingo_boards]:
-            for i in range(len(board)):
-                for j in range(len(board[0])):
+            for i in range(5):
+                for j in range(5):
                     if board[i][j] == v:
-                        board[i][j] = 'X'
-        
-            if check_board(board):
+                        board[i][j] = -1
+
+            if any([True for row in board if sum(row) == -5]) or \
+               any([True for col in zip(*board) if sum(col) == -5]):
                 bingo_boards.append((v, board))
 
 
 def calc_score(board):
-    return sum([int(elem) for row in board for elem in row if elem != 'X'])
+    return sum([int(elem) for row in board for elem in row if elem != -1])
 
 print(bingo_boards[0][0], calc_score(bingo_boards[0][1]), int(bingo_boards[0][0]) * calc_score(bingo_boards[0][1]))
 print(bingo_boards[-1][0], calc_score(bingo_boards[-1][1]), int(bingo_boards[-1][0]) * calc_score(bingo_boards[-1][1]))
