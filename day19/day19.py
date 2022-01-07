16655448a28318092ff56240eae4b13ea3fd69f8
Jack Pennington <jackcpennington@gmail.com>
2022-01-07 17:39:04 +0000

    day19 nothing

--- day19/day19.py
+++ day19/day19.py
@@ -1,4 +1,12 @@
-with open("day19/input", 'r') as f:
-    nums = [line for line in f.readlines()]
+import numpy as np
 
-print(nums)
+with open("day19/input_test", 'r') as f:
+    nums = [ np.array([np.array(list(map(int, coord.split(',')))) for coord in scanner.split('\n')[1:] ]) for scanner in f.read().split('\n\n')]
+
+# for num in nums:
+#     for coord in num:
+#         print(coord)
+#     print()
+
+# print(type(nums))
+
